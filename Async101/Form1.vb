dadd91f2b6418ec8f90c65621a2df73b1e6ba741
Jay Asbury <vbjay.net@gmail.com>
2020-06-28 18:08:19 -0400

    Show difference between gui and async delay

--- Async101/Form1.vb
+++ Async101/Form1.vb
@@ -4,12 +4,30 @@ Imports System.IO
 Public Class Form1
     Private WithEvents Log As New BindingList(Of LogMessageEventArgs)
     Private ReadOnly logStore As New Concurrent.ConcurrentQueue(Of LogMessageEventArgs)
+    Dim clicks As Guid = Guid.NewGuid
+    Private Sub btnGui_Click(sender As Object, e As EventArgs) Handles btnGui.Click
+        Dim id = tstAsync.Broadcast($"{NameOf(btnGui)} clicked.", clicks)
+        tstAsync.Wait10()
+        tstAsync.Broadcast($"{NameOf(btnGui)} click done.", clicks, id, True) ' excluding because this is work that contains all the other work.  
+    End Sub
+
+    Private Async Sub btnAsyncDelay_Click(sender As Object, e As EventArgs) Handles btnAsyncDelay.Click
+        Dim id = tstAsync.Broadcast($"{NameOf(btnAsyncDelay)} clicked.", clicks)
+        Await tstAsync.Wait10Async
+        tstAsync.Broadcast($"{NameOf(btnAsyncDelay)} done.", clicks, id, True) ' excluding because this is work that contains all the other work.  
+    End Sub
+
+    Private Sub tmrTime_Tick(sender As Object, e As EventArgs) Handles tmrTime.Tick
+        lblTime.Text = Now.ToLongTimeString
+    End Sub
+
     Private Sub tstAsync_BroadcastMessage(sender As Object, e As LogMessageEventArgs) Handles tstAsync.BroadcastMessage
         logStore.Enqueue(e)
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         MessageEventArgsBindingSource.DataSource = Log
+        tstAsync.Broadcast("Running in Gui thread.  Form Loaded")
     End Sub
 
     Sub UpdateLog(Optional EmptyLog As Boolean = False)
--- Async101/Tester.vb
+++ Async101/Tester.vb
@@ -1,5 +1,34 @@
+Imports System.Threading
+
 Public Class Tester
     Inherits ComponentModel.Component
+    ''' <summary>
+    ''' Runs in gui thread.  Blocks messages in windows message pump.  Timers don't run.  HEEEY the program is frozen.  This is why you do not do work in the gui thread.
+    ''' </summary>
+    Public Sub Wait10()
+        MessageBox.Show("This will lock the gui thread for 10 seconds. Try moving the window after hitting ok.  Note the time display freezes.Note the log doesn't update and the tmrLog does not run to dequeue the messages.")
+        Dim id As Guid = Guid.NewGuid
+        Dim strt = Broadcast("Calling Thread.Sleep", id)
+        Thread.Sleep(10000) ' do not use thread sleep in your code.  This forces the problem and is used here to demonstrate the issue of running long running code in the gui thread.
+        Broadcast("Thread.Sleep complete", id, strt)
+    End Sub
+
+    ''' <summary>
+    ''' Runs a task that waits 10 seconds to simulate work.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Async Function Wait10Async() As Task
+        Dim id As Guid = Guid.NewGuid
+        'in gui thread or at least the thread that is calling this method which happens to be the gui thread.  Read up on task schedulers in the tpl
+        Dim strt = Broadcast("Wait10 delay start now", id)
+
+        'spins up a task that completes after the delay
+        Await Task.Delay(10000)
+
+        'this is still run in the same context as the first line.  However it is called in a task continuation from the end of the delay call.  See https://ranjeet.dev/understanding-how-async-state-machine-works/
+        Broadcast("Wait10 delay stop now", id, strt)
+    End Function
+
 
     Public Function Broadcast(message As String, Optional ID As Guid? = Nothing, Optional EndID As Guid? = Nothing, Optional Exclude As Boolean = False) As Guid
 
